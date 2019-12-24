47e1982179411a1794bdd9b38b9b0830f946bb3d
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-12-24 10:21:48 +0530

    Changed config to java based rather than xml based

---
+++ src/main/java/com/medium/Config/Config.java
@@ -0,0 +1,21 @@
+package com.medium.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.medium.controllers"})
+public class Config {
+
+        @Bean
+        public InternalResourceViewResolver viewResolver() {
+            InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+            internalResourceViewResolver.setPrefix("/WEB-INF/");
+            internalResourceViewResolver.setSuffix(".jsp");
+
+            return  internalResourceViewResolver;
+        }
+
+    }
+
